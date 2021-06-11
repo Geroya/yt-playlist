e8dbcc8607356578d44a1e470a5f10427b70a6e4
Geroya <69263100+Geroya@users.noreply.github.com>
2021-06-11 21:47:43 +0200

    Update yt-playlist-download.py

--- yt-playlist-download.py
+++ yt-playlist-download.py
@@ -1,38 +1,27 @@
 import os
-import subprocess
+from pytube import Playlist
 
-from pytube import Playlist, YouTube
-
-def run(pl):
-    # get parent directory; VERY IMPORTANT!!
-    # INCLUDE LAST SLASH AFTER FOLDER NAME
-    # e.g. /home/username/Folder/ or C:\Users\Username\Folder\
-    filepath = input("Please enter the filepath of the directory where this script is located:\n")
-    # get linked list of links in the playlist
-    links = pl.parse_links()
-    # download each item in the list
-    for l in links:
-        # converts the link to a YouTube object
-        yt = YouTube(l)
-        # takes first stream; since ffmpeg will convert to mp3 anyway
-        music = yt.streams.first()
-        # gets the filename of the first audio stream
-        default_filename = music.default_filename
-        print("Downloading " + default_filename + "...")
-        # downloads first audio stream
-        music.download()
-        # creates mp3 filename for downloaded file
-        new_filename = default_filename[0:-3] + "mp3"
-        print("Converting to mp3....")
-        # converts mp4 audio to mp3 audio
-        subprocess.run(['ffmpeg', '-i', 
-            os.path.join(filepath, default_filename),
-            os.path.join(filepath, new_filename)
-        ])
+def downloadYTPlaylist():
+    path = input("Download Path: ")
+    pl = Playlist(input("Playlist URL: "))
+    print(f'Downloading: '+pl.title)
     
+    for video in pl.videos:
+        try:
+            videoFileName = video.title
+            print("Downloading " + videoFileName)
+            out_file = video.streams.filter(only_audio=True).first().download(output_path=path, max_retries=3)
+
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
+
+        except:
+            print("Something went wrong with video " + videoFileName)
+            continue
+
     print("Download finished.")
 
 if __name__ == "__main__":
-    url = input("Please enter the url of the playlist you wish to download: ")
-    pl = Playlist(url)
-    run(pl)
+    downloadYTPlaylist()
+
